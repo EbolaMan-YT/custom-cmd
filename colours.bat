65e01cd1354e11c3e11a3557a773ad8cda4d3f47
EbolaMan-YT <78039580+EbolaMan-YT@users.noreply.github.com>
2023-02-28 18:25:51 -0500

    Add files via upload

---
+++ colours.bat
@@ -0,0 +1,43 @@
+@echo off
+cls
+echo Bold
+echo.
+echo [1;90mGrey[0m
+echo [1;91mRed[0m
+echo [1;92mGreen[0m
+echo [93mYellow[0m
+echo [94mBlue[0m
+echo [95mMagenta[0m
+echo [96mCyan[0m
+echo [97mWhite[0m
+echo.
+echo Normal
+echo.
+echo [90mGrey[0m
+echo [91mRed[0m
+echo [92mGreen[0m
+echo [93mYellow[0m
+echo [94mBlue[0m
+echo [95mMagenta[0m
+echo [96mCyan[0m
+echo [97mWhite[0m
+echo [4mUnderlined[0m
+echo [7mHighlighted[0m
+echo [30mBlack[0m
+echo [31mDark Red[0m
+echo [32mDark Green[0m
+echo [33mDark Yellow[0m
+echo [34mDark Blue[0m
+echo [35mDark Purple[0m
+echo [36mDark Cyan[0m
+echo.
+echo [41mRed Highlight[0m
+echo [42mGreen Highlight[0m
+echo [43mYellow Highlight[0m
+echo [44mBlue Highlight[0m
+echo [45mPurple Highlight[0m
+echo [46mCyan Highlight[0m
+echo [47mCensor[0m
+echo.
+:end
+pause
